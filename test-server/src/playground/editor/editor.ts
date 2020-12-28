af3fcdb0464b526cb32ef8f106eb94ff7707293d
apalanevich <spalanevich@mail.ru>
2020-12-28 17:51:09 +0300

    Added dispatching custom event when markup inside editor is changed

--- test-server/src/playground/editor/editor.ts
+++ test-server/src/playground/editor/editor.ts
@@ -1,29 +1,44 @@
 import * as ace from 'brace';
 import 'brace/theme/monokai';
 import 'brace/mode/html';
+import {debounce} from '../../../../modules/esl-utils/async/debounce';
 
 export class PlaygroundEditor extends HTMLElement {
-  editor: ace.Editor;
+  protected editor: ace.Editor;
 
   constructor() {
     super();
+  }
+
+  set markup(val: string) {
+    this.editor.setValue(val);
+  }
+
+  get markup() {
+    return this.editor.getValue();
   }
 
   protected connectedCallback() {
     this.render();
     this.editor = ace.edit('editor');
     this.setEditorOptions();
+    this.editor.on('change', debounce(this.changeCallback.bind(this), 2000));
   }
 
   protected setEditorOptions() {
     this.editor.setOptions({
-      enableBasicAutocompletion: true,
-      enableSnippets: true,
-      enableLiveAutocompletion: false,
-      theme: 'ace/theme/twilight',
+      theme: 'ace/theme/monokai',
       mode: 'ace/mode/html',
       printMarginColumn: -1
     });
+  }
+
+  protected changeCallback() {
+    this.dispatchEvent(new CustomEvent('codeChange', {
+      bubbles: true,
+      detail: {markup: this.markup},
+      cancelable: true
+    }));
   }
 
   render() {
