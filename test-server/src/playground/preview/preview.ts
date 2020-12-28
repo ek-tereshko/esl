fac398101771c748bbd8b252dc9eee0d74910c2e
nharshunova@exadel.com <nharshunova@exadel.com>
2020-12-29 00:15:13 +0300

    [Playground]: Create Preview component

---
+++ test-server/src/playground/preview/preview.ts
@@ -0,0 +1,26 @@
+import {ESLBaseElement} from '../../../../src/modules/esl-base-element/core/esl-base-element';
+import {attr} from '../../../../src/modules/esl-base-element/decorators/attr';
+
+export class Preview extends ESLBaseElement {
+  static is = 'esl-preview';
+
+  @attr() public markup: string;
+
+  static get observedAttributes() {
+    return ['markup'];
+  }
+
+  protected connectedCallback() {
+    super.connectedCallback();
+    this.innerHTML = this.markup;
+  }
+
+  private attributeChangedCallback(attrName: string, oldVal: string, newVal: string) {
+    if (!this.connected || oldVal === newVal) return;
+    if (attrName === 'markup') {
+      this.innerHTML = newVal;
+    }
+  }
+}
+
+Preview.register();
