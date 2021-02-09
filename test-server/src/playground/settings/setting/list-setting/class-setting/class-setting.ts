3e4c6becb870510005627e3d911bf9d2ac0ff9e6
apalanevich <spalanevich@mail.ru>
2021-02-09 22:06:20 +0300

    feat(pages): add class settings on popup and accordion pages
    add class settings on popup and accordion pages

--- test-server/src/playground/settings/setting/list-setting/class-setting/class-setting.ts
+++ test-server/src/playground/settings/setting/list-setting/class-setting/class-setting.ts
@@ -1,5 +1,6 @@
 import {ESLListSetting} from '../list-setting';
 import {bind} from '../../../../../../../src/modules/esl-utils/decorators/bind';
+import {EventUtils} from '../../../../../../../src/modules/esl-utils/dom/events';
 
 export class ESLClassSetting extends ESLListSetting {
   public static is = 'esl-class-setting';
@@ -8,7 +9,8 @@ export class ESLClassSetting extends ESLListSetting {
   protected onValueChange(e: Event) {
     e.preventDefault();
     this.value = this.targetValue(e);
-    this.$$fire('classChange', {detail: {value: this.value, selector: this.selector, values: this.values}});
+    EventUtils.dispatch(this, 'classChange', {detail:
+        {value: this.value, selector: this.selector, values: this.values}});
   }
 }
 
