f2915d0a201835d8fa05827869769c871ad93205
apalanevich <spalanevich@mail.ru>
2021-02-15 10:52:26 +0300

    feat(editor): add editor's config through attributes
    add setting editor's config via editor-config attribute

--- test-server/src/playground/editor/config.ts
+++
@@ -1,6 +0,0 @@
-export const CONFIG = {
-  theme: 'ace/theme/chrome',
-  mode: 'ace/mode/html',
-  printMarginColumn: -1,
-  wrap: 120,
-};
--- test-server/src/playground/editor/editor.ts
+++ test-server/src/playground/editor/editor.ts
@@ -2,56 +2,80 @@ import * as ace from 'brace';
 import 'brace/theme/chrome';
 import 'brace/mode/html';
 import js_beautify from 'js-beautify';
-import {CONFIG} from './config';
 
 import {debounce} from '../../../../src/modules/esl-utils/async/debounce';
 import {bind} from '../../../../src/modules/esl-utils/decorators/bind';
 import {ESLPlayground} from '../core/playground';
-import {ESLBaseElement} from '../../../../src/modules/esl-base-element/core/esl-base-element';
+import {ESLBaseElement, jsonAttr} from '../../../../src/modules/esl-base-element/core';
 import {EventUtils} from '../../../../src/modules/esl-utils/dom/events';
+
+interface EditorConfig {
+  theme: string;
+  mode: string;
+  printMarginColumn: number;
+  wrap: number;
+}
 
 export class ESLEditor extends ESLBaseElement {
   public static is = 'esl-editor';
+  public static defaultOptions = {
+    theme: 'ace/theme/chrome',
+    mode: 'ace/mode/html',
+    printMarginColumn: -1,
+    wrap: 120,
+  };
+  @jsonAttr()
+  public editorConfig: EditorConfig;
+
   protected editor: ace.Editor;
   protected playground: ESLPlayground;
-  protected triggerChanges: boolean;
 
   protected connectedCallback(): void {
     super.connectedCallback();
     this.playground = this.closest(`${ESLPlayground.is}`) as ESLPlayground;
-    if (this.playground) {
-      this.playground.addEventListener('state:change', this.setMarkup);
-    }
+
+    if (!this.playground) return;
+
+    this.playground.addEventListener('state:change', this.setMarkup);
 
     this.editor = ace.edit(this);
     this.initEditorOptions();
 
     this.editor.$blockScrolling = Infinity;
-    this.editor.on('change', this.onChange);
+    this.editor.addEventListener('change', this.onChange);
+  }
+
+  protected disconnectedCallback() {
+    super.disconnectedCallback();
+    this.editor.removeEventListener('change', this.onChange);
+    this.playground && this.playground.removeEventListener('state:change', this.setMarkup);
+  }
+
+  protected get mergedEditorConfig() {
+    const type = (this.constructor as typeof ESLEditor);
+    return Object.assign({}, type.defaultOptions, this.editorConfig || {});
   }
 
   protected initEditorOptions(): void {
-    this.editor.setOptions(CONFIG);
+    this.editor.setOptions(this.mergedEditorConfig);
   }
 
   protected onChange = debounce(() => {
-    this.triggerChanges && EventUtils.dispatch(this, 'request:change', {detail: {source: ESLEditor.is, markup: this.editor.getValue()}});
-    this.triggerChanges = true;
+    EventUtils.dispatch(this, 'request:change', {detail: {source: ESLEditor.is, markup: this.editor.getValue()}});
   }, 1000);
 
   @bind
   protected setMarkup(e: CustomEvent): void {
     const {markup, source} = e.detail;
     if (source !== ESLEditor.is) {
-      this.triggerChanges = false;
-      this.editor.setValue(js_beautify.html(markup), -1);
+      this.setEditorValue(markup);
     }
   }
 
-  protected disconnectedCallback() {
-    super.disconnectedCallback();
-    this.editor.removeListener('change', this.onChange);
-    this.playground && this.playground.removeEventListener('state:change', this.setMarkup);
+  protected setEditorValue(value: string): void {
+    this.editor.removeEventListener('change', this.onChange);
+    this.editor.setValue(js_beautify.html(value), -1);
+    this.editor.addEventListener('change', this.onChange);
   }
 }
 
